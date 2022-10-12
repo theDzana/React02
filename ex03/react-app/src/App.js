3999c056dfca6fa9ecdd64553bcb0f2848f3e66a
Dedo-unuka <dzanazovko@gmail.com>
2022-10-12 14:11:07 +0200

    ex03

---
+++ ex03/react-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Plant from "./Plant";
+
+
+class App extends Component {
+  render() {
+      return (
+          <div>
+            <Plant />   
+          </div>
+      )
+  }
+}
+export default App;
+
---
+++ ex03/react-app/src/Plant.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Plant extends Component {
+    render() {
+        return (
+            <h1>This is a Plant Component!</h1>
+        )
+    }
+}
+
+export default Plant;
