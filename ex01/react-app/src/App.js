9190e8f4ff1f7e52bdbba1f60a7a638fa5232fc4
Dedo-unuka <dzanazovko@gmail.com>
2022-10-12 13:21:08 +0200

    ex01

---
+++ ex01/react-app/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class App extends React.Component {
+  render() {
+      return (
+          <div className="App">
+              <h1>Hello, React!</h1>
+          </div>
+      )
+  }
+}
+export default App;
